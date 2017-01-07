cc88a394928289b98c373e776625abc3420fbe30
丸山毅 <tac@sotai.co>
2017-01-07 23:48:09 +0900

    Eclipse上でRunできなかったので、方法変更。http://qiita.com/gishi_yama/items/748649b4d795cab5cbe6

---
+++ ml/src/main/scala/ml/util/Evaluater.scala
@@ -0,0 +1,19 @@
+package ml.util
+
+import ml.Data
+
+object Evaluater {
+
+  def evaluate(actualDataSet: Array[Data], predictDataSet: Array[Data]) {
+
+  }
+
+  private def report() {
+
+  }
+
+}
+
+class Evaluater(actualDataSet: Array[Data], predictDataSet: Array[Data]) {
+
+}
